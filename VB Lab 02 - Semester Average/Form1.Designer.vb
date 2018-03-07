1c517f61890e0feaefa20b2766840d926051a590
Tamas Marosan <tamas.marosan@dcmail.ca>
2018-03-07 01:07:13 -0500

    Add project files.

---
+++ VB Lab 02 - Semester Average/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SemesterAverageForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SemesterAverageForm))
+        Me.txtPercentA = New System.Windows.Forms.TextBox()
+        Me.lblGradeA = New System.Windows.Forms.Label()
+        Me.lblGradeB = New System.Windows.Forms.Label()
+        Me.txtPercentB = New System.Windows.Forms.TextBox()
+        Me.lblGradeC = New System.Windows.Forms.Label()
+        Me.txtPercentC = New System.Windows.Forms.TextBox()
+        Me.lblGradeD = New System.Windows.Forms.Label()
+        Me.txtPercentD = New System.Windows.Forms.TextBox()
+        Me.lblGradeE = New System.Windows.Forms.Label()
+        Me.txtPercentE = New System.Windows.Forms.TextBox()
+        Me.lblGradeF = New System.Windows.Forms.Label()
+        Me.txtPercentF = New System.Windows.Forms.TextBox()
+        Me.lblCourseA = New System.Windows.Forms.Label()
+        Me.lblCourseB = New System.Windows.Forms.Label()
+        Me.lblCourseC = New System.Windows.Forms.Label()
+        Me.lblCourseD = New System.Windows.Forms.Label()
+        Me.lblCourseE = New System.Windows.Forms.Label()
+        Me.lblCourseF = New System.Windows.Forms.Label()
+        Me.lblSem = New System.Windows.Forms.Label()
+        Me.lblGradeSem = New System.Windows.Forms.Label()
+        Me.lblPercentSem = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtPercentA
+        '
+        Me.txtPercentA.Location = New System.Drawing.Point(67, 6)
+        Me.txtPercentA.Name = "txtPercentA"
+        Me.txtPercentA.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentA.TabIndex = 1
+        Me.txtPercentA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGradeA
+        '
+        Me.lblGradeA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeA.Location = New System.Drawing.Point(153, 6)
+        Me.lblGradeA.Name = "lblGradeA"
+        Me.lblGradeA.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeA.TabIndex = 2
+        Me.lblGradeA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGradeB
+        '
+        Me.lblGradeB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeB.Location = New System.Drawing.Point(153, 32)
+        Me.lblGradeB.Name = "lblGradeB"
+        Me.lblGradeB.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeB.TabIndex = 5
+        Me.lblGradeB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPercentB
+        '
+        Me.txtPercentB.Location = New System.Drawing.Point(67, 32)
+        Me.txtPercentB.Name = "txtPercentB"
+        Me.txtPercentB.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentB.TabIndex = 4
+        Me.txtPercentB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGradeC
+        '
+        Me.lblGradeC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeC.Location = New System.Drawing.Point(153, 58)
+        Me.lblGradeC.Name = "lblGradeC"
+        Me.lblGradeC.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeC.TabIndex = 8
+        Me.lblGradeC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPercentC
+        '
+        Me.txtPercentC.Location = New System.Drawing.Point(67, 58)
+        Me.txtPercentC.Name = "txtPercentC"
+        Me.txtPercentC.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentC.TabIndex = 7
+        Me.txtPercentC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGradeD
+        '
+        Me.lblGradeD.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeD.Location = New System.Drawing.Point(153, 84)
+        Me.lblGradeD.Name = "lblGradeD"
+        Me.lblGradeD.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeD.TabIndex = 11
+        Me.lblGradeD.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPercentD
+        '
+        Me.txtPercentD.Location = New System.Drawing.Point(67, 84)
+        Me.txtPercentD.Name = "txtPercentD"
+        Me.txtPercentD.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentD.TabIndex = 10
+        Me.txtPercentD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGradeE
+        '
+        Me.lblGradeE.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeE.Location = New System.Drawing.Point(153, 110)
+        Me.lblGradeE.Name = "lblGradeE"
+        Me.lblGradeE.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeE.TabIndex = 14
+        Me.lblGradeE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPercentE
+        '
+        Me.txtPercentE.Location = New System.Drawing.Point(67, 110)
+        Me.txtPercentE.Name = "txtPercentE"
+        Me.txtPercentE.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentE.TabIndex = 13
+        Me.txtPercentE.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGradeF
+        '
+        Me.lblGradeF.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeF.Location = New System.Drawing.Point(153, 136)
+        Me.lblGradeF.Name = "lblGradeF"
+        Me.lblGradeF.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeF.TabIndex = 17
+        Me.lblGradeF.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPercentF
+        '
+        Me.txtPercentF.Location = New System.Drawing.Point(67, 136)
+        Me.txtPercentF.Name = "txtPercentF"
+        Me.txtPercentF.Size = New System.Drawing.Size(80, 20)
+        Me.txtPercentF.TabIndex = 16
+        Me.txtPercentF.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblCourseA
+        '
+        Me.lblCourseA.AutoSize = True
+        Me.lblCourseA.Location = New System.Drawing.Point(12, 9)
+        Me.lblCourseA.Name = "lblCourseA"
+        Me.lblCourseA.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseA.TabIndex = 0
+        Me.lblCourseA.Text = "Course &1:"
+        Me.lblCourseA.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblCourseB
+        '
+        Me.lblCourseB.AutoSize = True
+        Me.lblCourseB.Location = New System.Drawing.Point(12, 35)
+        Me.lblCourseB.Name = "lblCourseB"
+        Me.lblCourseB.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseB.TabIndex = 3
+        Me.lblCourseB.Text = "Course &2:"
+        Me.lblCourseB.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblCourseC
+        '
+        Me.lblCourseC.AutoSize = True
+        Me.lblCourseC.Location = New System.Drawing.Point(12, 61)
+        Me.lblCourseC.Name = "lblCourseC"
+        Me.lblCourseC.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseC.TabIndex = 6
+        Me.lblCourseC.Text = "Course &3:"
+        Me.lblCourseC.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblCourseD
+        '
+        Me.lblCourseD.AutoSize = True
+        Me.lblCourseD.Location = New System.Drawing.Point(12, 87)
+        Me.lblCourseD.Name = "lblCourseD"
+        Me.lblCourseD.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseD.TabIndex = 9
+        Me.lblCourseD.Text = "Course &4:"
+        Me.lblCourseD.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblCourseE
+        '
+        Me.lblCourseE.AutoSize = True
+        Me.lblCourseE.Location = New System.Drawing.Point(12, 113)
+        Me.lblCourseE.Name = "lblCourseE"
+        Me.lblCourseE.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseE.TabIndex = 12
+        Me.lblCourseE.Text = "Course &5:"
+        Me.lblCourseE.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblCourseF
+        '
+        Me.lblCourseF.AutoSize = True
+        Me.lblCourseF.Location = New System.Drawing.Point(12, 139)
+        Me.lblCourseF.Name = "lblCourseF"
+        Me.lblCourseF.Size = New System.Drawing.Size(52, 13)
+        Me.lblCourseF.TabIndex = 15
+        Me.lblCourseF.Text = "Course &6:"
+        Me.lblCourseF.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblSem
+        '
+        Me.lblSem.AutoSize = True
+        Me.lblSem.Location = New System.Drawing.Point(10, 165)
+        Me.lblSem.Name = "lblSem"
+        Me.lblSem.Size = New System.Drawing.Size(54, 13)
+        Me.lblSem.TabIndex = 18
+        Me.lblSem.Text = "Semester:"
+        Me.lblSem.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblGradeSem
+        '
+        Me.lblGradeSem.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGradeSem.Location = New System.Drawing.Point(153, 162)
+        Me.lblGradeSem.Name = "lblGradeSem"
+        Me.lblGradeSem.Size = New System.Drawing.Size(80, 20)
+        Me.lblGradeSem.TabIndex = 20
+        Me.lblGradeSem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPercentSem
+        '
+        Me.lblPercentSem.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPercentSem.Location = New System.Drawing.Point(67, 162)
+        Me.lblPercentSem.Name = "lblPercentSem"
+        Me.lblPercentSem.Size = New System.Drawing.Size(80, 20)
+        Me.lblPercentSem.TabIndex = 19
+        Me.lblPercentSem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(5, 187)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(228, 173)
+        Me.lblOutput.TabIndex = 21
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(158, 363)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 24
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(81, 363)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 23
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(4, 363)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 22
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'SemesterAverageForm
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(238, 391)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.lblPercentSem)
+        Me.Controls.Add(Me.lblSem)
+        Me.Controls.Add(Me.lblGradeSem)
+        Me.Controls.Add(Me.lblCourseF)
+        Me.Controls.Add(Me.lblCourseE)
+        Me.Controls.Add(Me.lblCourseD)
+        Me.Controls.Add(Me.lblCourseC)
+        Me.Controls.Add(Me.lblCourseB)
+        Me.Controls.Add(Me.lblCourseA)
+        Me.Controls.Add(Me.lblGradeF)
+        Me.Controls.Add(Me.txtPercentA)
+        Me.Controls.Add(Me.lblGradeE)
+        Me.Controls.Add(Me.txtPercentB)
+        Me.Controls.Add(Me.lblGradeD)
+        Me.Controls.Add(Me.txtPercentC)
+        Me.Controls.Add(Me.lblGradeC)
+        Me.Controls.Add(Me.txtPercentD)
+        Me.Controls.Add(Me.lblGradeB)
+        Me.Controls.Add(Me.txtPercentE)
+        Me.Controls.Add(Me.lblGradeA)
+        Me.Controls.Add(Me.txtPercentF)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(254, 430)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(254, 430)
+        Me.Name = "SemesterAverageForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Semester Grade Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPercentA As TextBox
+    Friend WithEvents lblGradeA As Label
+    Friend WithEvents lblGradeB As Label
+    Friend WithEvents txtPercentB As TextBox
+    Friend WithEvents lblGradeC As Label
+    Friend WithEvents txtPercentC As TextBox
+    Friend WithEvents lblGradeD As Label
+    Friend WithEvents txtPercentD As TextBox
+    Friend WithEvents lblGradeE As Label
+    Friend WithEvents txtPercentE As TextBox
+    Friend WithEvents lblGradeF As Label
+    Friend WithEvents txtPercentF As TextBox
+    Friend WithEvents lblCourseA As Label
+    Friend WithEvents lblCourseB As Label
+    Friend WithEvents lblCourseC As Label
+    Friend WithEvents lblCourseD As Label
+    Friend WithEvents lblCourseE As Label
+    Friend WithEvents lblCourseF As Label
+    Friend WithEvents lblSem As Label
+    Friend WithEvents lblGradeSem As Label
+    Friend WithEvents lblPercentSem As Label
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnCalculate As Button
+End Class
---
+++ VB Lab 02 - Semester Average/Form1.vb
@@ -0,0 +1,133 @@
+﻿'        Name: Tamas Marosan
+'     Program: Lab 02 - Semester Average
+'        Date: 2018-02-20
+' Description: Calculate the grade of a semester and assign a letter grade
+Option Strict On
+Public Class SemesterAverageForm
+
+    Const minGrade As Double = 0.0   ' Declare a constant for the minimum grade
+    Const maxGrade As Double = 100.0 ' Declare a constant for the maximum grade
+
+    ' When any textbox loses focus
+    Private Sub txtPercent_LostFocus() Handles txtPercentA.LostFocus, txtPercentB.LostFocus, txtPercentC.LostFocus, txtPercentD.LostFocus, txtPercentE.LostFocus, txtPercentF.LostFocus
+        lblOutput.ResetText() ' Reset the text for the output label
+
+        ' For each grade's textbox, and its corresponding label, perform input validation and assign a letter grade
+        validEntry(txtPercentA, lblGradeA)
+        validEntry(txtPercentB, lblGradeB)
+        validEntry(txtPercentC, lblGradeC)
+        validEntry(txtPercentD, lblGradeD)
+        validEntry(txtPercentE, lblGradeE)
+        validEntry(txtPercentF, lblGradeF)
+    End Sub
+
+    ' When the calculate button is used
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim gradeArray(5) As Double ' Declare the array for all given grades with a length of 6
+        Dim i As Integer = 0 ' Declare an index for the array
+        Dim gradeAverage As Double = 0.0 ' Declare a variable for the average grade
+        Dim allValid As Boolean = True ' Declare a boolean as true to continue the method if it's not set to false at some point
+        lblOutput.Text = $"ERROR(S):{vbCrLf}" ' Set the text of the output label to let the user know it's displaying errors (if there are any)
+
+        ' Revalidate each textbox before putting their values into the array
+        For Each txtBox As Object In Me.Controls ' Search through all objects in the form
+            If TypeOf (txtBox) Is TextBox Then ' If the object is a textbox
+                If CType(txtBox, TextBox).Text = String.Empty Or                   ' If any of the textboxes are empty 
+                Not Double.TryParse(CType(txtBox, TextBox).Text, gradeArray(i)) Or ' ...or not numeric
+                gradeArray(i) < minGrade Or gradeArray(i) > maxGrade Then          ' ...or not within range then...
+                    lblOutput.Text += $"Please ensure input in Course {i + 1} is a number between 0 and 100!{vbCrLf}" ' give an error for that textbox
+                    allValid = False ' ...and set a flag that not all textboxes are valid
+                Else ' If the textbox has valid input then...
+                    Double.TryParse(CType(txtBox, TextBox).Text, gradeArray(i)) ' insert the value of the textbox into the array
+                End If
+                i += 1 ' Use the next index for the next textbox
+            End If
+        Next ' Move onto the next object
+
+        If allValid = True Then ' If all of the textboxes had valid input, then...
+            For Each item As Double In gradeArray
+                gradeAverage += item / 6 ' obtain an average of all the grades
+            Next
+
+            lblPercentSem.Text = $"{Math.Round(gradeAverage, 2)}" ' change the semester percentage label to the average of the grades
+            lblGradeSem.Text = letterGrade(gradeAverage) ' Assign the corresponding grade letter to the semester letter grade label using the letterGrade function
+
+            ' Disable all textboxes on the form
+            For Each formObject As Object In Me.Controls ' Search all objects on the form
+                If TypeOf (formObject) Is TextBox Then ' If the object is a textbox
+                    CType(formObject, TextBox).Enabled = False ' Disable the textbox
+                End If
+            Next ' Move onto the next object
+            btnCalculate.Enabled = False ' Disable the Calculate button
+            lblOutput.ResetText() ' Reset the text for the output label
+        End If
+    End Sub
+
+    ' When the reset button is used
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        formReset() ' Call the formReset method
+    End Sub
+
+    ' When the exit button is used
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close this application if the Exit button is pressed
+        Application.Exit()
+    End Sub
+
+    ' Method to validate textbox input
+    Private Sub validEntry(ByVal currentTextBox As Object, ByVal currentLabel As Object)
+        Dim percentDouble As Double = 0.0 ' Declare a double for the textbox to be parsed into
+        CType(currentLabel, Label).Text = String.Empty ' reset the corresponding label to empty
+
+        If Not CType(currentTextBox, TextBox).Text = String.Empty Then ' If there is text in the textbox
+            If Not Double.TryParse(CType(currentTextBox, TextBox).Text, percentDouble) Then ' If the textbox isn't numeric
+                lblOutput.Text = $"Please ensure your input is numeric!{vbCrLf}" ' Give an approriate error
+
+                CType(currentTextBox, TextBox).Focus() ' Set the cursor back onto the textbox
+                CType(currentTextBox, TextBox).SelectAll() ' Highlight the text in the textbox to make it more streamlined for the user
+            ElseIf percentDouble < minGrade Or percentDouble > maxGrade Then ' If the input is not in range
+                lblOutput.Text = $"Please ensure your input is a number between 0 and 100!{vbCrLf}" ' Give an approriate error
+
+                CType(currentTextBox, TextBox).Focus() ' Set the cursor back onto the textbox
+                CType(currentTextBox, TextBox).SelectAll() ' Highlight the text in the textbox to make it more streamlined for the user
+            Else ' If the entry in the textbox is valid
+                CType(currentLabel, Label).Text = letterGrade(percentDouble) ' Use the letterGrade function to assign a letter grade to the corresponding label
+            End If
+        End If
+    End Sub
+
+    ' Function to assign a letter grade given a percentage grade
+    Private Function letterGrade(ByVal percentValue As Double) As String
+        Dim gradeLetter As String = "A+" ' Define a return variable as a string and initialize it to A+
+
+        ' Set the return variable to an appropriate grade depending on the percentage
+        If percentValue < 90 Then gradeLetter = "A"
+        If percentValue < 85 Then gradeLetter = "A-"
+        If percentValue < 80 Then gradeLetter = "B+"
+        If percentValue < 77 Then gradeLetter = "B"
+        If percentValue < 73 Then gradeLetter = "B-"
+        If percentValue < 70 Then gradeLetter = "C+"
+        If percentValue < 67 Then gradeLetter = "C"
+        If percentValue < 63 Then gradeLetter = "C-"
+        If percentValue < 60 Then gradeLetter = "D+"
+        If percentValue < 57 Then gradeLetter = "D"
+        If percentValue < 53 Then gradeLetter = "D-"
+        If percentValue < 50 Then gradeLetter = "F"
+
+        Return gradeLetter ' return the grade assigned when this function is called
+    End Function
+
+    ' Method to reset the form to its original state
+    Private Sub formReset()
+        For Each formObject As Object In Me.Controls ' Search through all objects in the form
+            If TypeOf (formObject) Is TextBox Then ' If the object is a textbox
+                CType(formObject, TextBox).ResetText() ' Reset the textbox's text
+                CType(formObject, TextBox).Enabled = True ' Enable the textbox
+            ElseIf TypeOf (formObject) Is Label AndAlso CType(formObject, Label).BorderStyle = BorderStyle.Fixed3D Then ' If the object is a label with a Fixed3D border
+                CType(formObject, Label).ResetText() ' Reset the label's text
+            End If
+        Next ' Move onto the next object
+        btnCalculate.Enabled = True ' Enable the calculate button
+        txtPercentA.Focus() ' Set the focus to the first textbox
+    End Sub
+End Class
